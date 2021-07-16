35cda1406cb61f5571ade647d4beffc8200799ae
Dildarov Suren <dildarovsuren@gmail.com>
2021-07-16 23:58:56 +0300

    Второе задание

---
+++ Numbers.java
@@ -0,0 +1,12 @@
+package com.max.idea;
+
+public class Numbers {
+    public static void main(String[] args) {
+
+        double a = 123.456;
+        System.out.println(a);
+
+        byte b = -17;
+        System.out.println(b);
+    }
+}
